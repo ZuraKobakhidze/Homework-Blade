64c714dd1ae4ccfeb8dcf1e6411ea9092a48b5e7
ZukaKobakhidze <107412747+ZukaKobakhidze@users.noreply.github.com>
2023-10-19 22:13:41 +0400

    feat: add js && css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', 'Welcome Page')
 
+@push('head')
+ <script src="{{ asset('main.js')}}"></script>
+ <link rel="stylesheet" href="{{asset('style.css')}}">
+@endpush
+
 @section ('content')
 <h1>Welcome content</h1>
 @endsection
